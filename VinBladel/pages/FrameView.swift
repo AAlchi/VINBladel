241787518549d96d9cb20b169fcf9578561e58d1
Jibbie0 <halaswolff07@gmail.com>
2024-05-31 09:01:38 -0500

    Fixed Camera?

--- VinBladel/pages/FrameView.swift
+++ VinBladel/pages/FrameView.swift
@@ -7,11 +7,10 @@
 
 import SwiftUI
 
-struct CameraView: View {
+struct FrameView: View {
     var image: CGImage?
-    private let label = Text("camera")
-    
-    var body: some View {
+    private let label = Text("frame")
+    var body: some View {4
         if let image = image {
             Image(image, scale: 1.0, orientation: .up, label: label)
         } else {
